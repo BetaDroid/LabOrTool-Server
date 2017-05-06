257b37b3273a712887e9c9da456e640cd9e84835
Daniel <danielzarioiu@gmail.com>
2017-05-06 11:07:38 +0200

    Implemented the manufacturer routes and functions.

---
+++ api/v1/manufacturer.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Daniel on 06/05/17.
+ */
+
+const router = require('../../configuration/router');
+const cn = require('../../configuration/db');
+
+router.get('/inventory/manufacturers/', function (req, res) {
+    cn.query('SELECT * FROM manufacturers;', function(err, rows) {
+        if (err) throw err;
+        else res.json(rows);
+    });
+});
+
+router.post('/inventory/manufacturers/', function (req, res) {
+    cn.query("INSERT INTO manufacturers (Name, Website) " +
+        "VALUES ('" + req.body.Name + "', '" + req.body.Website + "');", function(err, rows) {
+        if (err) throw err;
+        else res.json(true);
+    });
+});
+
+router.get('/inventory/manufacturers/:id', function (req, res) {
+    cn.query("SELECT * FROM manufacturers WHERE Id = "+ req.params.id +";", function(err, rows) {
+        if (err) throw err;
+        else res.json(rows[0]);
+    });
+});
+
+router.put('/inventory/manufacturers/:id', function (req, res) {
+    cn.query("UPDATE manufacturers SET Name = '" + req.body.Name + "', Website = '" + req.body.Website + "' " +
+        "WHERE Id = " + req.params.id + ";", function(err, rows) {
+        if (err) throw err;
+        else res.json(true);
+    });
+});
+
+router.delete('/inventory/manufacturers/:id', function (req, res) {
+    cn.query("DELETE FROM manufacturers WHERE Id = " + req.params.id + ";", function(err, rows) {
+        if (err) throw err;
+        else res.json(true);
+    });
+});
+
+router.get('/inventory/manufacturers/search/:text', function (req, res) {
+    cn.query("SELECT * FROM manufacturers WHERE Name LIKE '%"+ req.params.text +"%';", function(err, rows) {
+        if (err) throw err;
+        else res.json(rows);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ var app = express();
 */
 
 const api = require('./configuration/config');
-const distributor = require('./api/v1/distributors');
+const distributor = require('./api/v1/distributor');
+const manufacturer = require('./api/v1/manufacturer');
 
 api.use(distributor);
+api.use(manufacturer);
 
 const server = api.listen(8081, function () {
     const port = server.address().port;
