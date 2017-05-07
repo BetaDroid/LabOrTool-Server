56b33e40d0ad2501df0a0418df495240630360b2
Daniel <danielzarioiu@gmail.com>
2017-05-07 09:49:46 +0200

    Implemented the routes and some functions for the category-param-type.

---
+++ api/v1/category-param-type.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Daniel on 07/05/17.
+ */
+
+const router = require('../../configuration/router');
+const cn = require('../../configuration/db');
+
+router.get('/inventory/categories-param-types/', function (req, res) {
+    cn.query('SELECT * FROM categoriesparamtypes;', function(err, rows) {
+        if (err) throw err;
+        else res.json(rows);
+    });
+});
+
+router.post('/inventory/categories-param-types/', function (req, res) {
+    cn.query("INSERT INTO categoriesparamtypes (Name, CategoryId, UnitId, Order, Note) " +
+        "VALUES ('" + req.body.Name + "', " + req.body.CategoryId + ", " + req.body.UnitId + ", " +
+        "'" + req.body.Order + "', '" + req.body.Note + "');", function(err, rows) {
+        if (err) throw err;
+        else res.json(true);
+    });
+});
+
+router.get('/inventory/categories-param-types/:id', function (req, res) {
+    cn.query("SELECT * FROM categoriesparamtypes WHERE Id = "+ req.params.id +";", function(err, rows) {
+        if (err) throw err;
+        else res.json(rows[0]);
+    });
+});
+
+router.put('/inventory/categories-param-types/:id', function (req, res) {
+    cn.query("UPDATE categoriesparamtypes SET Name = '" + req.body.Name + "', CategoryId = " + req.body.CategoryId +
+        ", UnitId = " + req.body.UnitId + ", Order = '" + req.body.Order + "', Note = '" + req.body.Note + "' " +
+        "WHERE Id = " + req.params.id + ";", function(err, rows) {
+        if (err) throw err;
+        else res.json(true);
+    });
+});
+
+router.delete('/inventory/categories-param-types/:id', function (req, res) {
+    cn.query("DELETE FROM categoriesparamtypes WHERE Id = " + req.params.id + ";", function(err, rows) {
+        if (err) throw err;
+        else res.json(true);
+    });
+});
+
+router.get('/inventory/categories-param-types/search/:text', function (req, res) {
+    cn.query("SELECT `categoriesparamtypes`.`Name`, `categories`.`Name`, `units`.`Name`, `categoriesparamtypes`.`Order` " +
+        "FROM `categoriesparamtypes` " +
+        "INNER JOIN `categories` ON `categories`.`Id` = `categoriesparamtypes`.`CategoryId` " +
+        "INNER JOIN `units` ON `units`.`Id` = `categoriesparamtypes`.`UnitId`" +
+        "WHERE `categoriesparamtypes`.`Name` LIKE '%" + req.params.text + "%' OR " +
+        "`categories`.`Name` LIKE '%" + req.params.text + "%' OR " +
+        "`units`.`Name` LIKE '%" + req.params.text + "%' OR " +
+        "`categoriesparamtypes`.`Order` LIKE '%" + req.params.text + "%';", function(err, rows) {
+        if (err) throw err;
+        else res.json(rows);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const footprint = require('./api/v1/footprint');
 const category = require('./api/v1/category');
 const prefix = require('./api/v1/prefix');
 const unit = require('./api/v1/unit');
+const component = require('./api/v1/component');
+const cpt = require('./api/v1/category-param-type');
 
 api.use(distributor);
 api.use(manufacturer);
@@ -19,6 +21,8 @@ api.use(footprint);
 api.use(category);
 api.use(prefix);
 api.use(unit);
+api.use(component);
+api.use(cpt);
 
 const server = api.listen(8081, function () {
     const port = server.address().port;
