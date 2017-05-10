107151e643a9b0298a74dac53c341447e634bd13
Daniel <danielzarioiu@gmail.com>
2017-05-11 00:40:09 +0200

    Fixed one of the component-params route query.

--- api/v1/component-param.js
+++ api/v1/component-param.js
@@ -6,8 +6,8 @@ const router = require('../../configuration/router');
 const cn = require('../../configuration/db');
 
 router.get('/inventory/component-params/:id', function (req, res) {
-    cn.query("SELECT `componentparams`.`Id`, `componentparams`.`ComponentId`, `categoriesparamtypes`.`Name`, " +
-             "`componentparams`.`Value`, `prefixes`.`Name`, `componentparams`.`Note` " +
+    cn.query("SELECT `componentparams`.`Id`, `componentparams`.`ComponentId`, `categoriesparamtypes`.`Name` AS `CPT`, " +
+             "`componentparams`.`Value`, `prefixes`.`Name` AS `Prefix`, `componentparams`.`Note` " +
              "FROM `componentparams` " +
              "INNER JOIN `categoriesparamtypes` ON `categoriesparamtypes`.`Id` = `componentparams`.`CategoryParamTypeId` " +
              "INNER JOIN `prefixes` ON `prefixes`.`Id` = `componentparams`.`PrefixId` " +
@@ -24,18 +24,6 @@ router.post('/inventory/component-params/', function (req, res) {
         "'" + req.body.DistributorCode + "', " + req.body.Price + ", '" + req.body.Code +"', " + req.body.LocationId +
         ", '" + req.body.Datasheet + "', " + req.body.FootprintId + ", " + req.body.CategoryId + ", '" + req.body.Note +
         "');", function(err, rows) {
-        if (err) throw err;
-        else res.json(true);
-    });
-});
-
-router.put('/inventory/component-params/:id', function (req, res) {
-    cn.query("UPDATE components SET Name = '" + req.body.Name + "', ManufacturerId = " + req.body.ManufacturerId +
-        ", PartNumber = '" + req.body.PartNumber + "', DistributorId = " + req.body.DistributorId + ", " +
-        "DistributorCode = '" + req.body.DistributorCode + "', Price = " + req.body.Price + ", Code = '" +
-        req.body.Code +"', LocationId = " + req.body.LocationId + ", Datasheet = '" + req.body.Datasheet + "', " +
-        "FootprintId = " + req.body.FootprintId + ", CategoryId = " + req.body.CategoryId + ", Note = '" +
-        req.body.Note + "' WHERE Id = " + req.params.id + ";", function(err, rows) {
         if (err) throw err;
         else res.json(true);
     });
